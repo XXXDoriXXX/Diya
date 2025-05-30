ae5ddf89fdedbfad20de7758c117cd9714bc0d8b
Vadym <149205033+XXXDoriXXX@users.noreply.github.com>
2025-05-30 18:40:39 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -1,17 +1,17 @@
 // Активировать документы (true - включены, false - отключены)
-var isRightsEnabled = true; // Права
-var isCovidCertificateEnabled = true; // COVID-сертификат
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
 
 // Общее
-var birthdate = "01.01.2001"; // Дата рождения
-var fullname = "Фамилия Имя Отчество" // ФИО
-var name = "Имя" // Имя
+var birthdate = "27.04.2007"; // Дата рождения
+var fullname = "Коломієць Вадим Станіславович" // ФИО
+var name = "Вадим" // Имя
 
 // Паспорт
-var passport_id = "123456789" // Номер паспорта
+var passport_id = "3932010858" // Номер паспорта
 
 // КПП
-var kpp_id = "0123456789" // ИНН
+var kpp_id = "3932010858" // ИНН
 
 // Права
 var rights_valid_until = "01.01.2022" // Права "Дійсні до"
